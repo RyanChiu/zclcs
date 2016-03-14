a1ffee0e7641acf9c86bda8f55e210f9164afaf9
RyanChiu <ryan.chiu@hotmail.com>
2016-03-14 16:48:29 +0800

    hide cursor

--- zclcs.py
+++ zclcs.py
@@ -28,6 +28,7 @@ the main pgrogram here
 def main(stdscr):
 	curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
 	curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+	curses.curs_set(0)
 
 	# clear screen
 	stdscr.clear()
